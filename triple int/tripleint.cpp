bbc05738387e60a7ca3958e928ff5d0cd98751fa
sanbranogavyn <sanbranogavyn@gmail.com>
2026-01-13 17:53:30 -0800

    Add files via upload

---
+++ triple int/tripleint.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int number;
+
+    std::cout << "Enter an integer: ";
+    std::cin >> number;
+
+    int doubled = number * 3;
+
+    std::cout << "Doubled value: " << doubled << std::endl;
+
+    return 0;
+}
